a4c1aa2b5d1859b1b37f9afb3b060ae6694395ab
Lee-soha <https://github.com/Lee-soha>
2023-07-27 22:18:32 +0900

    add 404

--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 # Create your views here.
 articles = {
@@ -9,7 +9,12 @@ articles = {
 }
 
 def news_view(request, topic):
-    return HttpResponse(articles[topic])
+    try:
+        result = articles[topic]
+        return HttpResponse(articles[topic])
+    except :
+        result = "no page for that topic!"
+        raise Http404("404 Generic error") # 404.html
 
 def add_view(request, num1, num2):
     #domain.com/first_app/num1/num2 --> num1+num2
