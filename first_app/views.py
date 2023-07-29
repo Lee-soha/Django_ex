57d02999f40defb3f0192a0f29301c5fbf91fc89
Lee-soha <https://github.com/Lee-soha>
2023-07-29 19:25:08 +0900

    add redirection

--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.urls import reverse
+
+def simple_view(request):
+    return render(request,'first_app/example.html')
+
+
 
 # Create your views here.
 articles = {
@@ -15,6 +21,14 @@ def news_view(request, topic):
     except :
         result = "no page for that topic!"
         raise Http404("404 Generic error") # 404.html
+
+# domain.com/first_app/0 --> domamin.com/first_app/sport
+def num_page_view(request, num_page):
+
+    topics_list = list(articles.keys())
+    topic = topics_list[num_page]
+    return HttpResponseRedirect(reverse('topic-page', args=[topic]))
+
 
 def add_view(request, num1, num2):
     #domain.com/first_app/num1/num2 --> num1+num2
